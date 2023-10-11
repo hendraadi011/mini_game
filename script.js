9118bc6a72940bc18482b12d536ecc663941d2f7
hendraadi011 <hendraadisaputra011@gmail.com>
2023-10-11 09:13:30 +0700

    menambahkan tombol

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const canvas = document.querySelector('canvas');
 const board = canvas.getContext("2d");
+const jumpButton = document.getElementById('jump-button');
+const leftButton = document.getElementById('move-left');
+const rightButton = document.getElementById('move-right');
 
 canvas.width = DISPLAY_PIXEL.WIDTH * ASPECT_RATIO.WIDTH;
 canvas.height = DISPLAY_PIXEL.HEIGHT * ASPECT_RATIO.HEIGHT;
@@ -48,6 +51,23 @@ const animate = () =>  {
 
     window.requestAnimationFrame(animate)
 }
+
+// Tambahkan event listener untuk klik pada tombol
+jumpButton.addEventListener('click', function () {
+    player.jump(); // Ganti ini dengan aksi yang sesuai untuk melompat
+});
+
+
+// Tambahkan event listener untuk klik pada tombol
+leftButton.addEventListener('click', function () {
+    player.moveLeft();
+});
+
+rightButton.addEventListener('click', function() {
+    player.moveRight();
+})
+
+
 
 window.addEventListener('keydown', function (callback) {
     switch (callback.key) {
